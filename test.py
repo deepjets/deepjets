52524c80a08277149a3347a6aa8156c7a96de32f
Noel Dawe <noel.dawe@gmail.com>
2015-12-07 12:55:11 +1100

    improve example test script

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
 from deepjets import generate
-print list(generate(10, w_pt_min=250, w_pt_max=300))
+
+for leading_jet, subjets, constit in generate(100, w_pt_min=250, w_pt_max=300):
+    print leading_jet, subjets.shape[0], constit.shape[0]
