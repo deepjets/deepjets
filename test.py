4ccc075c5a6ef5334dde4ce333b6af0728610a0c
Noel Dawe <noel.dawe@gmail.com>
2015-12-07 23:08:29 +1100

    add qcd.config

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from deepjets import generate
 
-for leading_jet, subjets, constit in generate('wprime.config', 100,
+for leading_jet, subjets, constit in generate('wprime.config', 10,
                                              w_pt_min=250, w_pt_max=300):
+    print leading_jet, subjets.shape[0], constit.shape[0]
+
+for leading_jet, subjets, constit in generate('qcd.config', 10):
     print leading_jet, subjets.shape[0], constit.shape[0]
