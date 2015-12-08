7d1876c78b4525306439b2e083f51f878688e3d0
James Barnard <james.barnard@unimelb.edu.au>
2015-12-08 13:29:00 +1100

    Added PCA and reflections to JetPreprocessing

--- JetPreprocessing.py
+++ JetPreprocessing.py
@@ -34,19 +34,50 @@ def Pixelise(jet_csts, eta_edges, phi_edges):
                                                   weights=jet_csts[:,1])
     return pixels
     
-def RotateJet(pixels, slsubjet_centre=None):
-    """Return rotated and repixelised eta-phi histogram
+def RotateJet(pixels, slsubjet_centre=[0.0]):
+    """Return rotated and repixelised image array
     
        Rotation puts subleading subjet or first principle component at -pi/2
        Repixelisation interpolates with cubic spline"""
-    if slsubjet_centre == None:
-        theta = 0.0
-    elif slsubjet_centre[0] == 0.0:
+    if len(slsubjet_centre) == 2:
+        # Use subleading subject information
+        # Difference of 90 degrees due to pixel ordering
         theta = np.arctan2(slsubjet_centre[1], slsubjet_centre[0])
-    else:
-        theta = np.arctan2(slsubjet_centre[1], slsubjet_centre[0])
+        return rotate(pixels, -(theta*180.0/np.pi), order=3)
+
+    # Find principle component of image intensity
+    # Difference of 90 degrees due to pixel ordering
+    width, height  = pixels.shape
+    pix_coords     = np.array([ [i, j] for j in range(height-1, -height, -2)
+                                       for i in range(-width+1, width, 2) ])
+    covX           = np.cov( pix_coords, rowvar=0, aweights=np.reshape(pixels, (width*height)), bias=1 )
+    e_vals, e_vecs = np.linalg.eigh(covX)
+    pc             = e_vecs[:,-1]
+    theta          = np.arctan2(pc[1], pc[0])
+    r_pixels       = rotate(pixels, -(90+theta*180.0/np.pi), order=3)
     
-    return rotate(pixels, -theta*180.0/np.pi, order=3)
+    # Check orientation of principle component
+    pix_top = sum( r_pixels[:-(-height//2)].flatten() )
+    pix_bot = sum( r_pixels[(height//2):].flatten() )
+    if pix_top > pix_bot:
+        r_pixels = rotate(r_pixels, 180.0, order=3)
+    
+    return r_pixels
+
+def ReflectJet(pixels):
+    """Return reflected image array
+    
+       Reflection ensures right side of image has highest intensity"""
+    width, height  = pixels.shape
+    pix_l = sum( pixels[:,:-(-width//2)].flatten() )
+    pix_r = sum( pixels[:,(width//2):].flatten() )
+    if pix_l < pix_r:
+        return pixels
+    
+    r_pixels = np.array(pixels)
+    for i in range(width):
+        r_pixels[:,i] = pixels[:,-i]
+    return r_pixels
 
 def ShowJetImage(pixels, etaphi_range=(-1.25, 1.25, -1.25, 1.25)):
     fig  = plt.figure(figsize=(5, 5))
@@ -58,20 +89,13 @@ def ShowJetImage(pixels, etaphi_range=(-1.25, 1.25, -1.25, 1.25)):
     fig.savefig('AvJetImage.pdf', format='pdf')
     plt.close()
 
-"""    
-test_pix    = np.array([[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0],[0,0,0,0,0]])
-test_centre = np.array([-1,0])
-print RotateJet(test_pix, test_centre)  
-"""  
-
-os.environ['DYLD_LIBRARY_PATH'] = '/Users/barney800/Tools/fastjet/lib'
-
-etaphi_range = (-1.2, 1.2, -1.2, 1.2)
+  
+etaphi_range = (-1.25, 1.25, -1.25, 1.25)
 etaphi_delta = (0.1, 0.1)
 eta_edges    = np.arange(etaphi_range[0], 1.01*etaphi_range[1], etaphi_delta[0])
 phi_edges    = np.arange(etaphi_range[2], 1.01*etaphi_range[3], etaphi_delta[1])
-pixels       = np.zeros(( len(eta_edges) - 1, len(phi_edges) - 1 ))
-r_pixels     = np.zeros(( len(eta_edges) - 1, len(phi_edges) - 1 ))
+pixels       = np.zeros(( len(eta_edges)-1, len(phi_edges)-1 ))
+r_pixels     = np.zeros(( len(eta_edges)-1, len(phi_edges)-1 ))
 njets        = 1000
 generate     = False
 for i in range(njets):
@@ -88,8 +112,8 @@ for i in range(njets):
     if len(all_jets) > 2:
         slsubjet_centre = all_jets[2][1:3] - lsubjet_centre
     else:
-        slsubjet_centre = None
+        slsubjet_centre = [0.0]
     pixels   += jet_pixels
-    r_pixels += RotateJet(jet_pixels, slsubjet_centre)
+    r_pixels += ReflectJet(RotateJet(jet_pixels, slsubjet_centre))
 ShowJetImage(pixels / njets)
 ShowJetImage(r_pixels / njets)
