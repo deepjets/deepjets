97e9356f5ee5a3a9eb6cbf9adb73bcc6f971226e
Noel Dawe <noel.dawe@gmail.com>
2015-12-08 14:55:40 +1100

    update preprocessing

--- deepjets/preprocessing.py
+++ deepjets/preprocessing.py
@@ -15,16 +15,58 @@ def Pixelise(jet_csts, eta_edges, phi_edges):
                                                   weights=jet_csts[:,1])
     return pixels
 
-def RotateJet(pixels, slsubjet_centre=None):
-    """Return rotated and repixelised eta-phi histogram
+def RotateJet(pixels, slsubjet_centre=[0.0]):
+    """Return rotated and repixelised image array
 
        Rotation puts subleading subjet or first principle component at -pi/2
        Repixelisation interpolates with cubic spline"""
-    if slsubjet_centre is None:
-        theta = 0.0
-    elif slsubjet_centre[0] == 0.0:
+    if len(slsubjet_centre) == 2:
+        # Use subleading subject information
+        # Difference of 90 degrees due to pixel ordering
         theta = np.arctan2(slsubjet_centre[1], slsubjet_centre[0])
-    else:
-        theta = np.arctan2(slsubjet_centre[1], slsubjet_centre[0])
+        return rotate(pixels, -(theta*180.0/np.pi), order=3)
 
-    return rotate(pixels, -theta*180.0/np.pi, order=3)
+    # Find principle component of image intensity
+    # Difference of 90 degrees due to pixel ordering
+    width, height  = pixels.shape
+    pix_coords     = np.array([ [i, j] for j in range(height-1, -height, -2)
+                                       for i in range(-width+1, width, 2) ])
+    covX           = np.cov( pix_coords, rowvar=0, aweights=np.reshape(pixels, (width*height)), bias=1 )
+    e_vals, e_vecs = np.linalg.eigh(covX)
+    pc             = e_vecs[:,-1]
+    theta          = np.arctan2(pc[1], pc[0])
+    r_pixels       = rotate(pixels, -(90+theta*180.0/np.pi), order=3)
+
+    # Check orientation of principle component
+    pix_top = sum( r_pixels[:-(-height//2)].flatten() )
+    pix_bot = sum( r_pixels[(height//2):].flatten() )
+    if pix_top > pix_bot:
+        r_pixels = rotate(r_pixels, 180.0, order=3)
+
+    return r_pixels
+
+def ReflectJet(pixels):
+    """Return reflected image array
+
+       Reflection ensures right side of image has highest intensity"""
+    width, height  = pixels.shape
+    pix_l = sum( pixels[:,:-(-width//2)].flatten() )
+    pix_r = sum( pixels[:,(width//2):].flatten() )
+    if pix_l < pix_r:
+        return pixels
+
+    r_pixels = np.array(pixels)
+    for i in range(width):
+        r_pixels[:,i] = pixels[:,-i]
+    return r_pixels
+
+
+def preprocess(jets, subjets, constit, eta_edges, phi_edges):
+    lsubjet_centre     = subjets[0][1:3]
+    constit            = TranslateCsts(constit, lsubjet_centre)
+    jet_pixels         = Pixelise(constit, eta_edges, phi_edges)
+    if len(subjets) > 1:
+        slsubjet_centre = subjets[1][1:3] - lsubjet_centre
+    else:
+        slsubjet_centre = [0.0]
+    return ReflectJet(RotateJet(jet_pixels, slsubjet_centre))
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from deepjets import generate
-from deepjets.preprocessing import TranslateCsts, Pixelise, RotateJet
+from deepjets.preprocessing import preprocess
 from deepjets.utils import plot_jet_image
 
 etaphi_range = (-1.2, 1.2, -1.2, 1.2)
@@ -9,27 +9,15 @@ eta_edges    = np.arange(etaphi_range[0], 1.01*etaphi_range[1], etaphi_delta[0])
 phi_edges    = np.arange(etaphi_range[2], 1.01*etaphi_range[3], etaphi_delta[1])
 nevents = 5000
 
-
-def preprocess(leading_jet, subjets, constit):
-    lsubjet_centre = subjets[0][1:3]
-    jet_csts = TranslateCsts(constit, lsubjet_centre)
-    jet_pixels = Pixelise(constit, eta_edges, phi_edges)
-    if len(subjets) > 1:
-        slsubjet_centre = subjets[1][1:3] - lsubjet_centre
-    else:
-        slsubjet_centre = None
-    return RotateJet(jet_pixels, slsubjet_centre)
-
-
+# W'
 pixels = np.zeros(( len(eta_edges) - 1, len(phi_edges) - 1 ))
 for leading_jet, subjets, constit in generate('wprime.config', nevents,
                                              w_pt_min=250, w_pt_max=300):
-    pixels += preprocess(leading_jet, subjets, constit)
+    pixels += preprocess(leading_jet, subjets, constit, eta_edges, phi_edges)
 plot_jet_image(pixels / nevents, filename='wprime.png')
 
-
+# QCD dijets
 pixels = np.zeros(( len(eta_edges) - 1, len(phi_edges) - 1 ))
 for leading_jet, subjets, constit in generate('qcd.config', nevents):
-    pixels += preprocess(leading_jet, subjets, constit)
-
+    pixels += preprocess(leading_jet, subjets, constit, eta_edges, phi_edges)
 plot_jet_image(pixels / nevents, filename='qcd.png')
