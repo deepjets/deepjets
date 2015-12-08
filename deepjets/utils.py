1e8e97985899d07f691ab442199a1bddee66590c
Noel Dawe <noel.dawe@gmail.com>
2015-12-08 15:28:21 +1100

    plotting updates

--- deepjets/utils.py
+++ deepjets/utils.py
@@ -2,11 +2,12 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
 
-def plot_jet_image(pixels, etaphi_range=(-1.25, 1.25, -1.25, 1.25),
-                   filename='jet_image.png'):
+def plot_jet_image(pixels, eta_edges, phi_edges, filename='jet_image.png'):
+    eta_min, eta_max = eta_edges.min(), eta_edges.max()
+    phi_min, phi_max = phi_edges.min(), phi_edges.max()
     fig = plt.figure(figsize=(5, 5))
     ax = fig.add_subplot(111)
-    p = ax.imshow(pixels, extent=etaphi_range,
+    p = ax.imshow(pixels, extent=(eta_min, eta_max, phi_min, phi_max),
                   interpolation='none',
                   norm=LogNorm(vmin=1e-9, vmax=1e3))
     fig.colorbar(p, ax=ax)
--- test.py
+++ test.py
@@ -3,21 +3,19 @@ from deepjets import generate
 from deepjets.preprocessing import preprocess
 from deepjets.utils import plot_jet_image
 
-etaphi_range = (-1.2, 1.2, -1.2, 1.2)
-etaphi_delta = (0.1, 0.1)
-eta_edges    = np.arange(etaphi_range[0], 1.01*etaphi_range[1], etaphi_delta[0])
-phi_edges    = np.arange(etaphi_range[2], 1.01*etaphi_range[3], etaphi_delta[1])
-nevents = 5000
+eta_edges = np.linspace(-1.2, 1.2, 26)
+phi_edges = np.linspace(-1.2, 1.2, 26)
+nevents = 1000
 
 # W'
-pixels = np.zeros(( len(eta_edges) - 1, len(phi_edges) - 1 ))
+pixels = np.zeros((len(eta_edges) - 1, len(phi_edges) - 1))
 for leading_jet, subjets, constit in generate('wprime.config', nevents,
                                              w_pt_min=250, w_pt_max=300):
     pixels += preprocess(leading_jet, subjets, constit, eta_edges, phi_edges)
-plot_jet_image(pixels / nevents, filename='wprime.png')
+plot_jet_image(pixels / nevents, eta_edges, phi_edges, filename='wprime.png')
 
 # QCD dijets
-pixels = np.zeros(( len(eta_edges) - 1, len(phi_edges) - 1 ))
+pixels = np.zeros((len(eta_edges) - 1, len(phi_edges) - 1))
 for leading_jet, subjets, constit in generate('qcd.config', nevents):
     pixels += preprocess(leading_jet, subjets, constit, eta_edges, phi_edges)
-plot_jet_image(pixels / nevents, filename='qcd.png')
+plot_jet_image(pixels / nevents, eta_edges, phi_edges, filename='qcd.png')
