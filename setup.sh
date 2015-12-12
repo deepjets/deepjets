6fc79726936cdffad00cf70f73534a7d722c8e3a
Noel Dawe <noel.dawe@gmail.com>
2015-12-12 22:46:12 +1100

    support Mac OS in setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
-# change the following path if needed
+# change the following path if desired
 export DEEPJETS_SFT_DIR=~/software/hep
+
+# you should not need to edit below this line
 export PATH=${DEEPJETS_SFT_DIR}/bin:${PATH}
+if [ "$(uname)" == "Darwin" ]; then
+    export DYLD_LIBRARY_PATH=${DEEPJETS_SFT_DIR}/lib:${DYLD_LIBRARY_PATH}
+else
+    export LD_LIBRARY_PATH=${DEEPJETS_SFT_DIR}/lib:${LD_LIBRARY_PATH}
+fi
