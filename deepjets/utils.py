8ee562e62f96088827c197ebad0b81502ffff679
Noel Dawe <noel.dawe@gmail.com>
2015-12-18 00:48:28 +0000

    prevent RuntimeError from sqrt of neg

--- deepjets/utils.py
+++ deepjets/utils.py
@@ -10,12 +10,12 @@ def plot_jet_image(pixels, eta_edges, phi_edges,
     eta_min, eta_max = eta_edges.min(), eta_edges.max()
     phi_min, phi_max = phi_edges.min(), phi_edges.max()
     fig = plt.figure(figsize=(6, 5))
-    ax  = fig.add_subplot(111)
-    p   = ax.imshow(pixels, extent=(eta_min, eta_max, phi_min, phi_max),
-                    origin='low',
-                    interpolation='nearest',
-                    norm=LogNorm(vmin=vmin, vmax=vmax),
-                    cmap='jet')
+    ax = fig.add_subplot(111)
+    p = ax.imshow(pixels, extent=(eta_min, eta_max, phi_min, phi_max),
+                  origin='low',
+                  interpolation='nearest',
+                  norm=LogNorm(vmin=vmin, vmax=vmax),
+                  cmap='jet')
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.05)
     cbar = plt.colorbar(p, cax=cax, ticks=[1e-9,1e-7,1e-5,1e-3,1e-1,1e1,1e3])
@@ -29,8 +29,11 @@ def plot_jet_image(pixels, eta_edges, phi_edges,
 
 def jet_mass(jet_csts):
     """Returns jet mass calculated from constituent 4-vectors."""
-    E_tot  = np.sum( jet_csts['ET']*np.cosh(jet_csts['eta']) )
-    px_tot = np.sum( jet_csts['ET']*np.cos(jet_csts['phi']) )
-    py_tot = np.sum( jet_csts['ET']*np.sin(jet_csts['phi']) )
-    pz_tot = np.sum( jet_csts['ET']*np.sinh(jet_csts['eta']) )
-    return np.sqrt( E_tot**2-px_tot**2-py_tot**2-pz_tot**2 )
+    E_tot  = np.sum(jet_csts['ET'] * np.cosh(jet_csts['eta']))
+    px_tot = np.sum(jet_csts['ET'] * np.cos(jet_csts['phi']))
+    py_tot = np.sum(jet_csts['ET'] * np.sin(jet_csts['phi']))
+    pz_tot = np.sum(jet_csts['ET'] * np.sinh(jet_csts['eta']))
+    m2 = E_tot**2 - px_tot**2 - py_tot**2 - pz_tot**2
+    if m2 <= 0:
+        return 0.
+    return np.sqrt(E_tot**2 - px_tot**2 - py_tot**2 - pz_tot**2)
