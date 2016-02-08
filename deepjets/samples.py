3036b74b2c09f436337f74835a589325ce2fd6ad
Noel Dawe <noel.dawe@gmail.com>
2016-02-08 04:13:12 +0000

    save auxvars with generate_images and harmonize random_state

--- deepjets/samples.py
+++ deepjets/samples.py
@@ -1,6 +1,7 @@
 from joblib import Parallel, delayed
 import h5py
 import numpy as np
+from numpy.lib.recfunctions import append_fields
 
 from .generate import generate
 from .preprocessing import preprocess, pixel_edges
@@ -15,7 +16,16 @@ def get_images(config, nevents, pt_min, pt_max,
     Return image array and weights
     """
     images = np.empty((nevents, image_size, image_size), dtype=np.double)
+
     pt = np.empty(nevents, dtype=np.double)
+    pt_trimmed = np.empty(nevents, dtype=np.double)
+    mass = np.empty(nevents, dtype=np.double)
+    mass_trimmed = np.empty(nevents, dtype=np.double)
+    subjet_dr = np.empty(nevents, dtype=np.double)
+    tau_1 = np.empty(nevents, dtype=np.double)
+    tau_2 = np.empty(nevents, dtype=np.double)
+    tau_3 = np.empty(nevents, dtype=np.double)
+
     edges = pixel_edges(
         jet_size=jet_size,
         subjet_size_fraction=subjet_size_fraction,
@@ -25,17 +35,29 @@ def get_images(config, nevents, pt_min, pt_max,
     for event in generate(config, nevents, jet_size=jet_size,
                           subjet_size_fraction=subjet_size_fraction,
                           trimmed_pt_min=pt_min, trimmed_pt_max=pt_max,
+                          compute_auxvars=True,
                           **kwargs):
-        jets, subjets, constit, trimmed_constit, shrinkage = event
+        jets, subjets, constit, trimmed_constit, shrinkage, auxvars = event
         image = preprocess(subjets, trimmed_constit, edges,
                            zoom=1. / shrinkage,
                            normalize=normalize,
                            out_width=image_size)
         images[ievent] = image
-        # trimmed pT
-        pt[ievent] = jets[1]['pT']
+        pt[ievent] = jets[0]['pT']
+        pt_trimmed[ievent] = jets[1]['pT']
+        mass[ievent] = jets[0]['mass']
+        mass_trimmed[ievent] = jets[1]['mass']
+        subjet_dr[ievent] = auxvars['subjet_dr']
+        tau_1[ievent] = auxvars['tau_1']
+        tau_2[ievent] = auxvars['tau_2']
+        tau_3[ievent] = auxvars['tau_3']
         ievent += 1
-    return images, pt
+
+    auxvars = np.core.records.fromarrays(
+        [pt, pt_trimmed, mass, mass_trimmed, subjet_dr, tau_1, tau_2, tau_3],
+        names='pt,pt_trimmed,mass,mass_trimmed,subjet_dr,tau_1,tau_2,tau_3')
+
+    return images, auxvars
 
 
 def _generate_one_bin(config, nevents_per_pt_bin, pt_lo, pt_hi, **kwargs):
@@ -54,6 +76,7 @@ def get_sample(config, nevents_per_pt_bin, pt_min, pt_max,
     Construct a sample of images over a pT range by combining samples
     constructed in pT intervals in this range.
     """
+    random_state = kwargs.get('random_state', None)
     pt_bin_edges = np.linspace(pt_min, pt_max, pt_bins + 1)
 
     out = Parallel(n_jobs=n_jobs)(
@@ -61,7 +84,8 @@ def get_sample(config, nevents_per_pt_bin, pt_min, pt_max,
             for pt_lo, pt_hi in zip(pt_bin_edges[:-1], pt_bin_edges[1:]))
 
     images = np.concatenate([x[0] for x in out])
-    pt = np.concatenate([x[1] for x in out])
+    auxvars = np.concatenate([x[1] for x in out])
+    pt = auxvars['pt_trimmed']
 
     # Compute weights such that pT distribution is flat
     pt_hist, edges = np.histogram(pt, bins=np.linspace(pt_min, pt_max, (pt_bins * 4) + 1))
@@ -70,7 +94,16 @@ def get_sample(config, nevents_per_pt_bin, pt_min, pt_max,
     image_weights = np.true_divide(1., np.take(pt_hist, np.searchsorted(edges, pt) - 1))
     image_weights = np.true_divide(image_weights, image_weights.mean())
 
-    return images, pt, image_weights
+    # add weights column to auxvars
+    auxvars = append_fields(auxvars, 'weights', data=image_weights)
+
+    # shuffle
+    random_state = np.random.RandomState(random_state)
+    permute_idx = random_state.permutation(images.shape[0])
+    images = images[permute_idx]
+    auxvars = auxvars[permute_idx]
+
+    return images, auxvars
 
 
 def dataset_append(filename, datasetname, data):
