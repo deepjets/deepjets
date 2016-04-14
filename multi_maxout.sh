faa4dd244c1a60d12a20c83c41ff786b8f82fa20
James Barnard <james.barnard@unimelb.edu.au>
2016-04-14 22:43:46 +1000

    changed path

--- multi_maxout.sh
+++ multi_maxout.sh
@@ -10,8 +10,8 @@ python maxout_file_args.py $path/w_events_j1p0_sj0p50_jets_images.h5 \
 python maxout_file_args.py $path/w_events_j1p0_sj0p50_jets_zoomed_images.h5 \
                            $path/qcd_events_j1p0_sj0p50_jets_zoomed_images.h5 \
                            datasets/w_mwin_1-0p5_z models/w_mwin_1-0p5_z &
-python maxout_file_args.py $path/w_events_j1p0_sj0p50_jets_shrink_images.h5 \
-                           $path/qcd_events_j1p0_sj0p50_jets_shrink_images.h5 \
+python maxout_file_args.py $path/w_events_j1p0_sj0p50_shrink_jets_images.h5 \
+                           $path/qcd_events_j1p0_sj0p50_shrink_jets_images.h5 \
                            datasets/w_mwin_1-0p5_sz models/w_mwin_1-0p5_sz &
 
 python maxout_file_args.py $path/w_events_j1p0_sj0p30_jets_images.h5 \
@@ -20,8 +20,8 @@ python maxout_file_args.py $path/w_events_j1p0_sj0p30_jets_images.h5 \
 python maxout_file_args.py $path/w_events_j1p0_sj0p30_jets_zoomed_images.h5 \
                            $path/qcd_events_j1p0_sj0p30_jets_zoomed_images.h5 \
                            datasets/w_mwin_1-0p3_z models/w_mwin_1-0p3_z &
-python maxout_file_args.py $path/w_events_j1p0_sj0p30_jets_shrink_images.h5 \
-                           $path/qcd_events_j1p0_sj0p30_jets_shrink_images.h5 \
+python maxout_file_args.py $path/w_events_j1p0_sj0p30_shrink_jets_images.h5 \
+                           $path/qcd_events_j1p0_sj0p30_shrink_jets_images.h5 \
                            datasets/w_mwin_1-0p3_sz models/w_mwin_1-0p3_sz &
 
 python maxout_file_args.py $path/w_events_j1p2_sj0p20_jets_images.h5 \
@@ -30,6 +30,6 @@ python maxout_file_args.py $path/w_events_j1p2_sj0p20_jets_images.h5 \
 python maxout_file_args.py $path/w_events_j1p2_sj0p20_jets_zoomed_images.h5 \
                            $path/qcd_events_j1p2_sj0p20_jets_zoomed_images.h5 \
                            datasets/w_mwin_1p2-0p2_z models/w_mwin_1p2-0p2_z &
-python maxout_file_args.py $path/w_events_j1p2_sj0p20_jets_shrink_images.h5 \
-                           $path/qcd_events_j1p2_sj0p20_jets_shrink_images.h5 \
+python maxout_file_args.py $path/w_events_j1p2_sj0p20_shrink_jets_images.h5 \
+                           $path/qcd_events_j1p2_sj0p20_shrink_jets_images.h5 \
                            datasets/w_mwin_1p2-0p2_sz models/w_mwin_1p2-0p2_sz &
