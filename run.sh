0cf06ac886afd9804c7834b252af0b407bbb6e03
Noel Dawe <noel.dawe@gmail.com>
2016-05-02 15:23:42 +0000

    add run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+./generate-binned --events-per-bin 1000000 --random-state-offset 200 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 300 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 400 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 500 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 600 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 700 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 800 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 900 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 1000 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 1100 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+./generate-binned --events-per-bin 1000000 --random-state-offset 1200 --queue long qcd.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
+
+./generate-binned --events-per-bin 1000000 --random-state-offset 200 --queue long w.config /coepp/cephfs/mel/edawe/deepjets/events/pythia/binned/
