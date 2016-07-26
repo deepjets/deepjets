0f298ec085470dea57867afed0a4ebc4d7061284
Noel Dawe <noel.dawe@gmail.com>
2016-07-26 03:59:44 +0000

    add delphes_converter_card.tcl from Delphes/cards/converter_card.tcl

---
+++ delphes_converter_card.tcl
@@ -0,0 +1,18 @@
+#######################################
+# Order of execution of various modules
+#######################################
+
+set ExecutionPath {
+  TreeWriter
+}
+
+##################
+# ROOT tree writer
+##################
+
+module TreeWriter TreeWriter {
+# add Branch InputArray BranchName BranchClass
+# add Branch Delphes/allParticles Particle GenParticle
+  add Branch Delphes/stableParticles Particle GenParticle
+}
+
