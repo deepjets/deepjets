ea2693dde0bfe71539cf0c397b117cb0826b9135
Noel Dawe <noel.dawe@gmail.com>
2016-08-11 07:36:57 +0000

    disable uncertainties in pythia configs

--- test_showers.py
+++ test_showers.py
@@ -1,7 +1,7 @@
 from deepjets.generate import generate_events
 
-for event in generate_events('w_vincia.config', 1, write_to='vincia.hepmc', shower='vincia', random_state=1):
+for event in generate_events('w_vincia.config', 1, write_to='vincia.hepmc', shower='vincia', random_state=1, verbosity=0):
     pass
 
-for event in generate_events('w.config', 1, write_to='dire.hepmc', shower='dire', random_state=1):
-    pass
+for event, weight in generate_events('w.config', 1, write_to='dire.hepmc', shower='dire', random_state=1, verbosity=0):
+    print weight
